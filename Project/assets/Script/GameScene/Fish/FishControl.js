3811659320c91e4c797a2967482a609639f4de34
unknown <zhibing.zhou@studio.gamechef.cn>
2017-02-05 16:17:49 +0800

    +添加小羊动画，与移动。

--- Project/assets/Script/GameScene/Fish/FishControl.js
+++ Project/assets/Script/GameScene/Fish/FishControl.js
@@ -12,17 +12,30 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+        fish:cc.Node,
+        index:cc.int
     },
 
     // use this for initialization
     onLoad: function () {
-        
+
     },
     
-    onButtonEnterGameScene:function()
+    update:function()
     {
-        cc.director.loadScene('GameScene');
+        this.move();
+    },
+    
+    move:function()
+    {
+        if(this.fish == null)
+            return;
+        //this.fish.position.x += 10;
+        this.fish.position = cc.v2(this.fish.position.x - 3  , this.fish.position.y);
+        //console.log(" this.fish.position.x  = " +  this.fish.position.x )
+        
     }
+
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
 
--- Project/assets/Script/GameScene/GameSceneControl.js
+++ Project/assets/Script/GameScene/GameSceneControl.js
@@ -12,12 +12,18 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+        
+        content: cc.Node  
     },
 
     // use this for initialization
     onLoad: function () 
     {
-        
+        cc.loader.loadRes("Prefable/Fish_1", (err , prefable) => {
+            var node = cc.instantiate(prefable);
+            this.content.addChild(node);
+            node.position = cc.v2(910, 110);
+        });
     },
 
     // called every frame, uncomment this function to activate update callback
